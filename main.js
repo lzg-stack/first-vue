310c4613c18f8ed7c965ed5531f1d0d86e25d457
lzg-stack <64410808+lzg-stack@users.noreply.github.com>
2020-11-11 19:29:45 -0500

    v-bind

--- main.js
+++ main.js
@@ -2,7 +2,10 @@ const app = Vue.createApp({
     data() {
         return {
             product: 'Socks',
-            description: 'auskgdb',
+            image: './assets/images/socks_blue.jpg',
+            // solution
+            url: 'https://www.vuemastery.com/'
+            // solution
         }
     }
 })
