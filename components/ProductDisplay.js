b95a1418ebe760305caf8fe0a54cd22dcdff11ef
lzg-stack <64410808+lzg-stack@users.noreply.github.com>
2020-11-12 15:29:13 -0500

    communication

--- components/ProductDisplay.js
+++ components/ProductDisplay.js
@@ -1,54 +1,78 @@
 app.component('product-display', {
     props: {
-        premium: {
-            type: Boolean,
-            required: true
-        }
+      premium: {
+        type: Boolean,
+        required: true
+      }
     },
-    template:
-        /*html*/
-        `<div class="product-display">
-    <div class="product-container">
-      <div class="product-image">
-        <img v-bind:src="image">
-      </div>
-      <div class="product-info">
-        <h1>{{ title }}</h1>
-        <p>{{ sale }}</p>
-
-        <p v-if="inStock">In Stock</p>
-        <p v-else>Out of Stock</p>
-
-        <p>Shipping: {{ shipping }}</p>
-
-        <product-details :details="details"></product-details>
-
-        <div class="color-circle" v-for="(variant,index) in variants" :key="variant.id"
-          @mouseover="updateVariant(index)" :style="{ backgroundColor: variant.color }">
+    template: 
+    /*html*/
+    `<div class="product-display">
+      <div class="product-container">
+        <div class="product-image">
+          <img v-bind:src="image">
         </div>
-        <button class="button" :class="{ disabledButton: !inStock }" :disabled="!inStock" v-on:click="addToCart">Add
-          to Cart</button>
+        <div class="product-info">
+          <h1>{{ title }}</h1>
+  
+          <p v-if="inStock">In Stock</p>
+          <p v-else>Out of Stock</p>
+  
+          <p>Shipping: {{ shipping }}</p>
+          <ul>
+            <li v-for="detail in details">{{ detail }}</li>
+          </ul>
+  
+          <div 
+            v-for="(variant, index) in variants" 
+            :key="variant.id" 
+            @mouseover="updateVariant(index)" 
+            class="color-circle" 
+            :style="{ backgroundColor: variant.color }">
+          </div>
+          
+          <button 
+            class="button" 
+            :class="{ disabledButton: !inStock }" 
+            :disabled="!inStock" 
+            v-on:click="addToCart">
+            Add to Cart
+          </button>
+  
+          <!-- solution -->
+          <button 
+          class="button" 
+          :class="{ disabledButton: !inStock }" 
+          :disabled="!inStock" 
+          @click="removeFromCart">
+          Remove Item
+        </button>
+        <!-- solution -->
+  
+        </div>
       </div>
-    </div>
-  </div>`,
+    </div>`,
     data() {
-        return {
-            product: 'Socks',
-            brand: 'Vue Mastery',
-            selectedVariant: 0,
-            image: './assets/images/socks_blue.jpg',
-            details: ['50% cotton', '30% wool', '20% polyester'],
-            variants: [
-                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
-                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 },
-            ],
-            onSale: true,
-        }
+      return {
+          product: 'Socks',
+          brand: 'Vue Mastery',
+          selectedVariant: 0,
+          details: ['50% cotton', '30% wool', '20% polyester'],
+          variants: [
+            { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
+            { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 },
+          ]
+      }
     },
     methods: {
         addToCart() {
-            this.cart += 1
+            this.$emit('add-to-cart', this.variants[this.selectedVariant].id)
         },
+        // solution
+        removeFromCart() {
+          this.$emit('remove-from-cart', this.variants[this.selectedVariant].id)
+        },
+        // solution
         updateVariant(index) {
             this.selectedVariant = index
         }
@@ -61,19 +85,13 @@ app.component('product-display', {
             return this.variants[this.selectedVariant].image
         },
         inStock() {
-            return this.variants[this.selectedVariant].image
+            return this.variants[this.selectedVariant].quantity
         },
-        sale() {
-            if (this.onSale) {
-                return this.brand + ' ' + this.product + ' is on sale. '
-            }
-            return ' '
-        },
-        shipping(){
-            if(this.premium){
-                return 'Free'
-            }
-            return 2.99
+        shipping() {
+          if (this.premium) {
+            return 'Free'
+          }
+          return 2.99
         }
     }
-})
+  })
