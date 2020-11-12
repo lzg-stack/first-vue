05959a66e41a4a20cd7edd55ff61e6f90ea7f98d
lzg-stack <64410808+lzg-stack@users.noreply.github.com>
2020-11-11 20:08:01 -0500

    v-for

--- main.js
+++ main.js
@@ -2,10 +2,19 @@ const app = Vue.createApp({
     data() {
         return {
             product: 'Socks',
-            image: './assets/images/socks_blue.jpg',
-            // solution
-            url: 'https://www.vuemastery.com/'
-            // solution
+            image: './assets/images/socks_green.jpg',
+            url: 'https://www.vuemastery.com/',
+
+            inventory: 100,
+            onSale: true,
+            details: ['50% cotton', '30% wool', '20% polyester'],
+
+            variants: [
+                { id: 1234, color: 'green' },
+                { id: 3456, color: 'yellow' }
+            ],
+
+            sizes: ['S', 'M', 'L', 'XL']
         }
     }
 })
